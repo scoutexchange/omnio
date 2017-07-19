6e8e9076ac11937b88839d5e53251f83e4bcc1c4
Bob Green <rgreen@goscoutgo.com>
2017-07-19 17:49:40 -0400

    Move io module into s3
    The Reader side of s3 is very generic, but since there's no other
    supported schemes for now, it is simpler organizationally to
    keep it all in the s3 module since that's the only place it is
    used for the time being.

--- omnio/s3.py
+++ omnio/s3.py
@@ -2,9 +2,84 @@ import urllib
 
 import boto3
 
-from omnio import io
-
 UPLOAD_PART_SIZE = 5 * 1024**2
+
+
+class Reader:
+    def __init__(self, stream):
+        self.stream = stream
+        self.line_iter = _iter_lines(self, chunk_size=512)
+        self.closed = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        self.close()
+
+    def __iter__(self):
+        self._ensure_open()
+
+        return self
+
+    def __next__(self):
+        self._ensure_open()
+
+        return next(self.line_iter)
+
+    def _ensure_open(self):
+        if self.closed:
+            msg = 'I/O operation on a closed file'
+            raise ValueError(msg)
+
+    def close(self):
+        self.closed = True
+
+    def flush(self):  # pragma: no cover
+        pass
+
+    def readable(self):  # pragma: no cover
+        return True
+
+    def seekable(self):  # pragma: no cover
+        return False
+
+    def writable(self):  # pragma: no cover
+        return False
+
+    def read(self, size=None):
+        self._ensure_open()
+
+        data = self.stream.read(size)
+        return data
+
+    def seek(self, offset, whence=0):  # pragma: no cover
+        raise NotImplementedError()
+
+
+def _iter_lines(stream, chunk_size):
+    pending = None
+    while True:
+        chunk = stream.read(chunk_size)
+        if not chunk:
+            break
+
+        if pending is not None:
+            chunk = pending + chunk
+
+        lines = chunk.splitlines(keepends=True)
+
+        # The last item in the list is typically an unfinished line
+        # that needs to be completed with the next chunk. For
+        # expediency, we treat it as incomplete until the next
+        # chunk's splitlines() proves otherwise.
+        pending = lines.pop()
+
+        for line in lines:
+            yield line
+
+    if pending is not None:
+        yield pending
 
 
 class Writer:
@@ -108,7 +183,7 @@ def open_(uri, mode):  # pragma: no cover
         resp = s3.get_object(Bucket=bucket, Key=key)
         stream = resp['Body']
 
-        return io.Reader(stream)
+        return Reader(stream)
 
     if 'w' in mode:
         return Writer(s3, bucket, key)
