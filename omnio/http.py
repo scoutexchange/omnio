09e5cf9c3e1f31e2992e1579863b10b14a4f2c5a
Bob Green <rgreen@goscoutgo.com>
2017-07-20 10:31:39 -0400

    Inherit http reader from io.IOBase
    This gets rid of some boiler plate as well as a custom line iterator
    function.

--- omnio/http.py
+++ omnio/http.py
@@ -1,43 +1,17 @@
+import io
 import requests
 
 
-class HTTPReader:
+class HTTPReader(io.IOBase):
     def __init__(self, resp):
         self.content_iter = resp.iter_content(chunk_size=512,
                                               decode_unicode=False)
-        self.line_iter = _iter_lines(self.content_iter)
         self.buffer = bytearray()
-        self.closed = False
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type_, value, traceback):
-        self.close()
-
-    def __iter__(self):
-        self._ensure_open()
-
-        return self
-
-    def __next__(self):
-        self._ensure_open()
-
-        return next(self.line_iter)
-
-    def _ensure_open(self):
+    def read(self, size=None):
         if self.closed:
             msg = 'I/O operation on a closed file'
             raise ValueError(msg)
-
-    def close(self):
-        self.closed = True
-
-    def flush(self):
-        pass
-
-    def read(self, size=None):
-        self._ensure_open()
 
         while True:
             if size is not None and len(self.buffer) >= size:
@@ -53,40 +27,10 @@ class HTTPReader:
         data = self.buffer[:size]
         self.buffer = self.buffer[size:]
 
-        return data
+        return bytes(data)
 
     def readable(self):
         return True
-
-    def seekable(self):
-        return False
-
-    def writable(self):
-        return False
-
-    def seek(self, offset, whence=0):  # pragma: no cover
-        raise NotImplementedError()
-
-
-def _iter_lines(content_iter):
-    pending = None
-    for chunk in content_iter:
-        if pending is not None:
-            chunk = pending + chunk
-
-        lines = chunk.splitlines(keepends=True)
-
-        # The last item in the list is typically an unfinished line
-        # that needs to be completed with the next chunk. For
-        # expediency, we treat it as incomplete until the next
-        # chunk's splitlines() proves otherwise.
-        pending = lines.pop()
-
-        for line in lines:
-            yield line
-
-    if pending is not None:
-        yield pending
 
 
 def open_(uri, mode):  # pragma: no cover
