225e22c7859a0a63dcf9f1e353831c563a588712
Bob Green <rgreen@goscoutgo.com>
2018-01-23 11:16:14 -0500

    Add module summary to for help()

--- omnio/__init__.py
+++ omnio/__init__.py
@@ -1,3 +1,16 @@
+"""
+This library provides the `omnio.open()` function with an interface
+very similar to that of the built in Python `open` function.  The
+difference is that while Python's `open` operates only on local
+filesystem paths, `omnio` accepts URIs as well.
+
+In addition, it supports compression and decompression of streams
+with gzip or bz2.
+
+It currently supports `file`, `http`, and `s3` URIs, though it may
+be expanded to support additional schemes in the future.
+"""
+
 # Internally, we define the main open function as open_ to avoid
 # shadowing the python built-in open(). However, the module API
 # is designed to be semantically referenced as omnio.open().  So
