f1198056d33aa7e7fc6c5b4e0d3418dfd9e0a8c4
Bob Green <rgreen@aquent.com>
2022-01-22 11:11:15 -0500

    Remove unnecessary os.path.join

--- omnio/path.py
+++ omnio/path.py
@@ -1,8 +1,6 @@
-import os
 import urllib
 
 
 def _open(uri, mode, config):
     parsed_uri = urllib.parse.urlparse(uri)
-    os.path.abspath(os.path.join(parsed_uri.netloc, parsed_uri.path))
     return open(parsed_uri.path, mode)
